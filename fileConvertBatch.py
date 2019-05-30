bced408ef019d8231b0a3342afb3025f8c6ac997
Eric Minor <ermi1253@colorado.edu>
2019-05-29 21:45:36 -0600

    fixed bugs, added size

--- fileConvertBatch.py
+++ fileConvertBatch.py
@@ -2,7 +2,9 @@ import glob
 import os
 from fileConvert import fileConvert
 
-targetDir = "C:\\Users\\Eric Minor\\TrackingML\\defectTracker\\labels"
+
+imgDims = [100,100]
+targetDir = "E:/Projects/fake/simulations/fortran\LandauGin/run20190529_144637/data-k-1.00-beta-10.000-mu-0.000"
 outDir = "C:\\Users\\Eric Minor\\TrackingML\\defectTracker\\labels\\out"
 
 if not os.path.exists(outDir):
@@ -11,5 +13,5 @@ if not os.path.exists(outDir):
 filePattern = os.path.join(targetDir, '*.txt')   
 
 for filename in glob.glob(filePattern):
-    fileConvert(filename,headerLines=1)
+    fileConvert(filename,headerLines=1,imgSize=imgDims)
 
--- fileConvertF.py
+++ fileConvertF.py
@@ -4,7 +4,12 @@ from xml.dom import minidom
 # Import the CSV into numpy array
 def fileConvert(filen,outDir):
 #filen = 'circle_1.txt'
-    print(filen)
+
+    splitFile = filen.split('\\')
+    splitName = splitFile[-1].split('.')
+    justName = splitName[0]
+    imName = justName+".jpg"
+    #print(filen)
     defects = np.loadtxt(open(filen,'rb'), delimiter=' ', skiprows=1)
     defects = defects.astype(int)
 
@@ -19,7 +24,7 @@ def fileConvert(filen,outDir):
     folder.appendChild(doc.createTextNode('images'))
     annotation.appendChild(folder)
     filename = doc.createElement('filename')
-    filename.appendChild(doc.createTextNode(filen[:-4]))
+    filename.appendChild(doc.createTextNode(imName))
     annotation.appendChild(filename)
     segmented = doc.createElement('segmented')
     segmented.appendChild(doc.createTextNode('0'))
@@ -36,57 +41,106 @@ def fileConvert(filen,outDir):
     size.appendChild(depth)
     annotation.appendChild(size)
 
+    #print(len(defects.shape))
+    if len(defects.shape)>1:
+        for line in defects:
+          # Create defect properties
+          xmin = doc.createElement('xmin')
+          xmax = doc.createElement('xmax')
+          ymin = doc.createElement('ymin')
+          ymax = doc.createElement('ymax')
 
-    for line in defects:
-      # Create defect properties
-      xmin = doc.createElement('xmin')
-      xmax = doc.createElement('xmax')
-      ymin = doc.createElement('ymin')
-      ymax = doc.createElement('ymax')
+          # Create branch elements to be used
+          objet = doc.createElement('object')
+          bndbox = doc.createElement('bndbox')
+          name = doc.createElement('name')
+          pose = doc.createElement('pose')
+          truncated = doc.createElement('truncated')
+          difficult = doc.createElement('difficult')
+          
+          # Add text data to the elements
+          name.appendChild(doc.createTextNode('class'))
+          pose.appendChild(doc.createTextNode('center'))
+          truncated.appendChild(doc.createTextNode('1'))
+          difficult.appendChild(doc.createTextNode('0'))
+          
+          if line.size == 2:
+            xmin.appendChild(doc.createTextNode(str(line[0]-1)))
+            xmax.appendChild(doc.createTextNode(str(line[0]+1)))
+            ymin.appendChild(doc.createTextNode(str(line[1]-1)))
+            ymax.appendChild(doc.createTextNode(str(line[1]+1)))
+          elif line.size == 4:
+            xmin.appendChild(doc.createTextNode(str(line[0])))
+            xmax.appendChild(doc.createTextNode(str(line[2])))
+            ymin.appendChild(doc.createTextNode(str(line[1]-1)))
+            ymax.appendChild(doc.createTextNode(str(line[3]+1)))
+          else:
+            print(line)
+          bndbox.appendChild(xmin)
+          bndbox.appendChild(xmax)
+          bndbox.appendChild(ymin)
+          bndbox.appendChild(ymax)
 
-      # Create branch elements to be used
-      objet = doc.createElement('object')
-      bndbox = doc.createElement('bndbox')
-      name = doc.createElement('name')
-      pose = doc.createElement('pose')
-      truncated = doc.createElement('truncated')
-      difficult = doc.createElement('difficult')
-      
-      # Add text data to the elements
-      name.appendChild(doc.createTextNode('class'))
-      pose.appendChild(doc.createTextNode('center'))
-      truncated.appendChild(doc.createTextNode('1'))
-      difficult.appendChild(doc.createTextNode('0'))
-      
-      if line.size == 2:
-        xmin.appendChild(doc.createTextNode(str(line[0]-1)))
-        xmax.appendChild(doc.createTextNode(str(line[0]+1)))
-        ymin.appendChild(doc.createTextNode(str(line[1]-1)))
-        ymax.appendChild(doc.createTextNode(str(line[1]+1)))
-      elif line.size == 4:
-        xmin.appendChild(doc.createTextNode(str(line[0])))
-        xmax.appendChild(doc.createTextNode(str(line[2])))
-        ymin.appendChild(doc.createTextNode(str(line[1]-1)))
-        ymax.appendChild(doc.createTextNode(str(line[3]+1)))
-      bndbox.appendChild(xmin)
-      bndbox.appendChild(xmax)
-      bndbox.appendChild(ymin)
-      bndbox.appendChild(ymax)
-      
-      # Add the elements to the defect
-      objet.appendChild(name)
-      objet.appendChild(pose)
-      objet.appendChild(truncated)
-      objet.appendChild(difficult)
-      objet.appendChild(bndbox)
+          
+          # Add the elements to the defect
+          objet.appendChild(name)
+          objet.appendChild(pose)
+          objet.appendChild(truncated)
+          objet.appendChild(difficult)
+          objet.appendChild(bndbox)
 
-      # Add the defect to the overall list
-      annotation.appendChild(objet)
+          # Add the defect to the overall list
+          annotation.appendChild(objet)
+
+    else: 
+        line = defects
+        xmin = doc.createElement('xmin')
+        xmax = doc.createElement('xmax')
+        ymin = doc.createElement('ymin')
+        ymax = doc.createElement('ymax')
+
+        # Create branch elements to be used
+        objet = doc.createElement('object')
+        bndbox = doc.createElement('bndbox')
+        name = doc.createElement('name')
+        pose = doc.createElement('pose')
+        truncated = doc.createElement('truncated')
+        difficult = doc.createElement('difficult')
+
+        # Add text data to the elements
+        name.appendChild(doc.createTextNode('class'))
+        pose.appendChild(doc.createTextNode('center'))
+        truncated.appendChild(doc.createTextNode('1'))
+        difficult.appendChild(doc.createTextNode('0'))
+
+        if line.size == 2:
+            xmin.appendChild(doc.createTextNode(str(line[0]-1)))
+            xmax.appendChild(doc.createTextNode(str(line[0]+1)))
+            ymin.appendChild(doc.createTextNode(str(line[1]-1)))
+            ymax.appendChild(doc.createTextNode(str(line[1]+1)))
+        elif line.size == 4:
+            xmin.appendChild(doc.createTextNode(str(line[0])))
+            xmax.appendChild(doc.createTextNode(str(line[2])))
+            ymin.appendChild(doc.createTextNode(str(line[1]-1)))
+            ymax.appendChild(doc.createTextNode(str(line[3]+1)))
+        else:
+            print(line)
+        bndbox.appendChild(xmin)
+        bndbox.appendChild(xmax)
+        bndbox.appendChild(ymin)
+        bndbox.appendChild(ymax)
 
 
-    splitFile = filen.split('\\')
-    splitName = splitFile[-1].split('.')
-    justName = splitName[0]
+        # Add the elements to the defect
+        objet.appendChild(name)
+        objet.appendChild(pose)
+        objet.appendChild(truncated)
+        objet.appendChild(difficult)
+        objet.appendChild(bndbox)
+
+        # Add the defect to the overall list
+        annotation.appendChild(objet)        
+
       
     xml_str = doc.toprettyxml(indent="  ")
     outputFile = outDir+"\\"+justName+".xml"
