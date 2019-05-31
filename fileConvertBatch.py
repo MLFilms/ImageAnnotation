8cc0694139d002a7471210e2b0fb0e533dfd47b8
Eric Minor <ermi1253@colorado.edu>
2019-05-30 22:40:05 -0600

    small

--- fileConvertBatch.py
+++ fileConvertBatch.py
@@ -2,16 +2,14 @@ import glob
 import os
 from fileConvert import fileConvert
 
+def fileConvertBatch(targetDir,imgDims):
+    print(targetDir)
+    filePattern = os.path.join(targetDir, '*.txt')   
 
-imgDims = [100,100]
-targetDir = "E:/Projects/fake/simulations/fortran\LandauGin/run20190529_144637/data-k-1.00-beta-10.000-mu-0.000"
-outDir = "C:\\Users\\Eric Minor\\TrackingML\\defectTracker\\labels\\out"
+    for filename in glob.glob(filePattern):
+        fileConvert(filename,headerLines=1,imgSize=imgDims)
 
-if not os.path.exists(outDir):
-    os.makedirs(outDir)
-
-filePattern = os.path.join(targetDir, '*.txt')   
-
-for filename in glob.glob(filePattern):
-    fileConvert(filename,headerLines=1,imgSize=imgDims)
-
+if __name__ == "__main__":
+    imgDims = [100,100]
+    targetDir = "E:/Projects/fake/simulations/fortran\LandauGin/run20190529_144637/data-k-1.00-beta-10.000-mu-0.000"
+    fileConvertBatch(targetDir,imgDims)
